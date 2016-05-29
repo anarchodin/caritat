5e16c8fc96015a286e04f1bba0db79eba9aad70b
Herbert Snorrason <odin@anarchism.is>
2016-05-29 18:46:04 +0000

    Added test tools.
    Added some testing tools. First test case added, related to the Meek bug from the previous
    commit. This will require some further work. :)

---
+++ test/meek.js
@@ -0,0 +1,23 @@
+var caritat = require('../index');
+var Election = caritat.Election;
+var meek = caritat.stv.meek;
+
+var expect = require('chai').expect;
+
+describe('Single Transferable Vote', function () {
+  describe('Meek\'s Method', function () {
+    it('should give the right number of seats', function () {
+      var ballots = [
+        ["Jóhann","Indriði","Kjartan"],
+        ["Indriði","Jóhann","Kjartan"],
+        ["Indriði","Kjartan","Jóhann"],
+        ["Kjartan","Indriði","Jóhann"]
+      ];
+      var election = new Election();
+      ballots.forEach(x => election.addBallot(x));
+
+      expect(meek(election, {seats: 2})).to.have.length(2);
+
+    });
+  });
+});
