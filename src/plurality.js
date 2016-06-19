6ffa670ba51e0c774ff9bab186a627e26c04a18c
Herbert Snorrason <odin@anarchism.is>
2016-06-19 15:35:59 +0000

    Allow plurality to give more seats.
    Although plurality is strictly a single-winner system, allow it to
    give several results in an ordered array. This makes it consistent
    with approval voting.

--- src/plurality.js
+++ src/plurality.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
 import {countFirstPrefs} from './utils';
 
-function winner (election) {
+function winner (election, config) {
   let ballots = election.ballots;
   let candidates = election.candidates;
+
+  config = _.defaults(config, {
+    seats: 1
+  });
 
   var firstPrefs = countFirstPrefs(ballots, candidates);
 
   var sorted = _.orderBy(_.toPairs(firstPrefs), x => { return x[1] }, ['desc']);
 
-  return _.map(sorted, x => { return x[0] })[0];
+  return _.take(_.map(sorted, x => { return x[0] }), config.seats);
 }
 
 export default winner;
